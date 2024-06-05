8e48965c51e43a3a39d570c49f23d4105f554c1f
Luca Martorelli <lucam0109@gmail.com>
2024-06-05 11:50:00 +0200

    Improved PutCacheMapTest with JaCoCo and BaDua
    Things are apparently going good, don't wanna say nothing till is working :pray:

--- openjpa-kernel/src/test/java/org/apache/openjpa/util/PutCacheMapTest.java
+++ openjpa-kernel/src/test/java/org/apache/openjpa/util/PutCacheMapTest.java
@@ -23,6 +23,8 @@ public class PutCacheMapTest {
     private final ValueType stateOfValue;
     private final Object existingValue;
     private final boolean expectedNull;
+    private final boolean keyPinned;
+
 
 
 
@@ -41,6 +43,7 @@ public class PutCacheMapTest {
 
     public PutCacheMapTest(EntriesTuple entriesTuple) {
         this.stateOfKey = entriesTuple.stateOfKey();
+        this.keyPinned = entriesTuple.keyPinned();
         this.stateOfValue = entriesTuple.stateOfValue();
         this.existingValue = new Object();
         this.expectedNull = entriesTuple.isNull();
@@ -50,29 +53,39 @@ public class PutCacheMapTest {
     public static Collection<EntriesTuple> getReadInputTuples() {
         List<EntriesTuple> putEntriesList = new ArrayList<>();
 
-        putEntriesList.add(new EntriesTuple(KeyType.Null,              ValueType.ValidObject,  false));  //case 1
-        putEntriesList.add(new EntriesTuple(KeyType.Null,              ValueType.Null,         true));   //case 2
-        putEntriesList.add(new EntriesTuple(KeyType.Null,              ValueType.InvalidObject,false));  //case 3 Strange case
-        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject,  ValueType.ValidObject,  false));  //case 4
-        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject,  ValueType.Null,         true));   //case 5
-        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject,  ValueType.InvalidObject,false));  //case 6 Strange case
-        putEntriesList.add(new EntriesTuple(KeyType.InvalidObject,     ValueType.ValidObject,  true));   //case 7
-        putEntriesList.add(new EntriesTuple(KeyType.InvalidObject,     ValueType.Null,         true));   //case 8
-        putEntriesList.add(new EntriesTuple(KeyType.InvalidObject,     ValueType.InvalidObject,true));   //case 9
-        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,     ValueType.ValidObject,  false));  //case 10
-        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,     ValueType.Null,         true));   //case 11
-        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,     ValueType.InvalidObject,false));  //case 12 Strange case
+        putEntriesList.add(new EntriesTuple(KeyType.Null,             ValueType.ValidObject,  false,false)); //case 1
+        putEntriesList.add(new EntriesTuple(KeyType.Null,             ValueType.Null,         true, false)); //case 2
+        putEntriesList.add(new EntriesTuple(KeyType.Null,             ValueType.InvalidObject,false,false)); //case 3
+        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject, ValueType.ValidObject,  false,false)); //case 4
+        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject, ValueType.Null,         true, false)); //case 5
+        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject, ValueType.InvalidObject,false,false)); //case 6
+        putEntriesList.add(new EntriesTuple(KeyType.InvalidObject,    ValueType.ValidObject,  true, false)); //case 7
+        putEntriesList.add(new EntriesTuple(KeyType.InvalidObject,    ValueType.Null,         true, false)); //case 8
+        putEntriesList.add(new EntriesTuple(KeyType.InvalidObject,    ValueType.InvalidObject,true, false)); //case 9
+        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,    ValueType.ValidObject,  false,false)); //case 10
+        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,    ValueType.Null,         true, false)); //case 11
+        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,    ValueType.InvalidObject,false,false)); //case 12
+
+        //New tests added so to upgrade coverage (after BA-DUA and JaCoCo report)
+        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,    ValueType.ValidObject,  false,true));  //case 13
+        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,    ValueType.Null,         true, true));  //case 14
+        putEntriesList.add(new EntriesTuple(KeyType.ExistedObject,    ValueType.InvalidObject,false,true));  //case 15
+        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject, ValueType.ValidObject,  false,true));  //case 16
+        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject, ValueType.Null,         true, true));  //case 17
+        putEntriesList.add(new EntriesTuple(KeyType.NotExistedObject, ValueType.InvalidObject,false,true));  //case 18
 
         return putEntriesList;
     }
 
     private static final class EntriesTuple {
         private final KeyType keyType;
+        private final boolean keyPinned;
         private final ValueType valueType;
         private final boolean nullValue;
 
-        private EntriesTuple(KeyType stateOfKey, ValueType stateOfValue, boolean nullValue) {
+        private EntriesTuple(KeyType stateOfKey, ValueType stateOfValue, boolean nullValue, boolean keyPin) {
             this.keyType = stateOfKey;
+            this.keyPinned = keyPin;
             this.valueType = stateOfValue;
             this.nullValue = nullValue;
         }
@@ -86,6 +99,10 @@ public class PutCacheMapTest {
         }
         public boolean isNull() {
             return nullValue;
+        }
+
+        public boolean keyPinned() {
+            return keyPinned;
         }
     }
 
@@ -123,6 +140,11 @@ public class PutCacheMapTest {
     @Test
     public void testPut() {
 
+        // Se la chiave è da pin, pinna la chiave prima di metterla nella mappa
+        if (keyPinned && key != null) {
+            cacheMap.pin(key);
+        }
+
         Object retVal = this.cacheMap.put(this.key, this.value);
 
         System.out.println("\n______________________________________________");
@@ -134,11 +156,16 @@ public class PutCacheMapTest {
         System.out.println("Cached Value: " + this.cacheMap.get(this.key));
 
         if(this.expectedNull){
-            System.out.println(this.cacheMap.get(this.key)==null);
+            System.out.println(this.cacheMap.get(this.key) == null);
             Assert.assertNull(this.cacheMap.get(this.key));
-
         } else {
             Assert.assertEquals(this.value, this.cacheMap.get(this.key));
+        }
+
+        // Verifica che l'elemento rimanga nella cache anche se è pin
+        if (keyPinned && key != null) {
+            Assert.assertTrue(cacheMap.getPinnedKeys().contains(key));
+            Assert.assertEquals(this.value, cacheMap.get(this.key));
         }
     }
 }
